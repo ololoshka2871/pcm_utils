775474e9022a3aaf693ff916a21fe64cfa514bf7
shiloxyz <shiloxyz@q6600arch>
2020-06-06 18:42:48 +0300

    update pcm_utils/MyTwoDimArray.h

--- pcm_utils/MyTwoDimArray.h
+++ pcm_utils/MyTwoDimArray.h
@@ -1,33 +1,30 @@
 #pragma once
 
-#include <cstdint>
 #include <cassert>
+#include <cstdint>
 
-template <typename T>
-class myTwoDimArray {
+template <typename T> class myTwoDimArray {
 public:
-	myTwoDimArray(void* data, const int32_t width, const int32_t heigth)
-		: data(reinterpret_cast<T*>(data)), width(width), heigth(heigth) {	}
+  myTwoDimArray(void *data, const int32_t width, const int32_t heigth)
+      : data(reinterpret_cast<T *>(data)), width(width), heigth(heigth) {}
 
-	T& element(int32_t x, int32_t y) {
-		assert(x < width && y < heigth);
-		return getLine(y)[x];
-	}
+  void setData(T *newptr) { data = newptr; }
 
-	int32_t getwidth() const {
-		return width;
-	}
+  T &element(int32_t x, int32_t y) {
+    assert(x < width && y < heigth);
+    return getLine(y)[x];
+  }
 
-	int32_t getheigth() const {
-		return heigth;
-	}
+  int32_t getwidth() const { return width; }
 
-	T* getLine(int32_t y) const {
-		assert(y < heigth);
-		return &data[y * width];
-	}
+  int32_t getheigth() const { return heigth; }
+
+  T *getLine(int32_t y) const {
+    assert(y < heigth);
+    return &data[y * width];
+  }
 
 private:
-	T* data;
-	const int32_t width, heigth;
+  T *data;
+  const int32_t width, heigth;
 };
