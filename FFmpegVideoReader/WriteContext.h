c691a532392e50cc5e563718d5cd5dc2e909fa24
shiloxyz <shiloxyz@q6600arch>
2020-06-12 15:17:08 +0300

    savve

--- FFmpegVideoReader/WriteContext.h
+++ FFmpegVideoReader/WriteContext.h
@@ -1,52 +1,53 @@
 #pragma once
 
-extern "C"
-{
+#include <string>
+
+extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
-#include <libswresample/swresample.h>
-#include <libswscale/swscale.h>
 #include <libavformat/avio.h>
-#include <libavutil/pixdesc.h>
-#include <libavutil/hwcontext.h>
-#include <libavutil/opt.h>
 #include <libavutil/avassert.h>
+#include <libavutil/frame.h>
+#include <libavutil/hwcontext.h>
 #include <libavutil/imgutils.h>
 #include <libavutil/motion_vector.h>
-#include <libavutil/frame.h>
+#include <libavutil/opt.h>
+#include <libavutil/pixdesc.h>
+#include <libswresample/swresample.h>
+#include <libswscale/swscale.h>
 }
 
 struct OutputStream {
-	AVStream *st;
-	AVCodecContext *enc;
+  AVStream *st;
+  AVCodecContext *enc;
 
-	/* pts of the next frame that will be generated */
-	int64_t next_pts;
-	int samples_count;
+  /* pts of the next frame that will be generated */
+  int64_t next_pts;
+  int samples_count;
 
-	AVFrame *frame;
+  AVFrame *frame;
 
-	float t, tincr, tincr2;
+  float t, tincr, tincr2;
 
-	struct SwsContext *sws_ctx;
-	struct SwrContext *swr_ctx;
+  struct SwsContext *sws_ctx;
+  struct SwrContext *swr_ctx;
 };
 
 struct FfmpegWriteContext {
-	FfmpegWriteContext(const std::string & filename,
-		const std::string & codec_name, int32_t heigth, int32_t width, int64_t bitrate,
-		bool PAL, int32_t src_buf_width);
-	~FfmpegWriteContext();
+  FfmpegWriteContext(const std::string &filename, const std::string &codec_name,
+                     int32_t heigth, int32_t width, int64_t bitrate, bool PAL,
+                     int32_t src_buf_width);
+  ~FfmpegWriteContext();
 
-	void encode_frame(uint8_t* pixel_data);
+  void encode_frame(uint8_t *pixel_data);
 
 private:
-	AVFormatContext *oc;
-	AVOutputFormat *fmt;
-	AVCodec* codec;
-	AVDictionary *opt;
-	AVFrame *tframe;
-	SwsContext *sws_ctx;
+  AVFormatContext *oc;
+  AVOutputFormat *fmt;
+  AVCodec *codec;
+  AVDictionary *opt;
+  AVFrame *tframe;
+  SwsContext *sws_ctx;
 
-	OutputStream video_st;
+  OutputStream video_st;
 };
